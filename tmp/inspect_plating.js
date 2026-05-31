e6f28c243b8590a2a88f08873c9ae7cb9a1e605e
Oxygen Founder <founder@oxybio.in>
2026-05-31 07:55:35 +0530

    security: remove .env.production and tmp/ from tracking, add to .gitignore

--- tmp/inspect_plating.js
+++
@@ -1,5 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-supabase.from('lab_notebook_entries').select('stage_snapshots').neq('stage_snapshots', null).then(r => {
-  console.log(r);
-});
